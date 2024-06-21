f28b3601bd3175ab39ff71ea11564a9e84107e36
gf23yu4vfyvbdyh <okada_yu@micware.co.jp>
2024-06-21 11:03:02 +0900

    Add files via upload

---
+++ page_script.js
@@ -0,0 +1,9 @@
+function butotnClick2() {
+    console.log("テストログ0002")
+}
+
+let button2 = document.getElementById('btn_1');
+console.log("テストログ0001")
+button2.addEventListener('click', butotnClick2);
+
+
